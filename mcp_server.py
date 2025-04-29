1ec5cc0fe1cd540b1e547cd2c22cbe646d09fc23
Haitam BORQANE <haitam.borqane@innovx.ma>
2025-04-29 16:17:04 +0100

    add tools to the MCP server

--- mcp_server.py
+++ mcp_server.py
@@ -17,5 +17,34 @@ def add(a: int, b: int) -> int:
     print(f"Adding {a} and {b}")
     return a + b
 
+@mcp.tool()
+def multiplication(a: int, b: int) -> int:
+    """Multiply 2 numbers
+
+    Args:
+        a (int): first number
+        b (int): second number
+
+    Returns:
+        int: Multiplication
+    """
+    print(f"Multiplying {a} and {b}")
+    return a * b
+
+@mcp.tool()
+def divide(a: int, b:int) -> int:
+    """Divide 2 numbers a by b
+
+    Args:
+        a (int): first number
+        b (int): second number
+
+    Returns:
+        int: Division
+    """
+
+    print(f"Dividing {a} by {b}")
+    return a / b   
+
 if __name__ == "__main__":
     mcp.run(transport="sse")
